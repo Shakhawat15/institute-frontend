cf5cec6343005a92922808d1c084c750554d4d11
Md Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2025-10-05 02:33:32 +0600

    update base url

--- src/API/config.js
+++ src/API/config.js
@@ -1,9 +1,9 @@
 import { getToken } from "../helper/SessionHelper";
 
-const baseURL = "http://127.0.0.1:8000/api/v1";
+// const baseURL = "http://127.0.0.1:8000/api/v1";
 const imageBaseURL = "http://localhost:8000";
 
-// const baseURL = "https://fruits-backend-ten.vercel.app/api/v1";
+const baseURL = "https://institute-backend.vercel.app/api/v1";
 
 const AxiosHeader = { headers: { Authorization: getToken() } };
 const AxiosHeaderForImage = {
